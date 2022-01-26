48c94a3fcdd81d51ef29830f8034c336e8324896
Marco Strijker <marco.strijker@ortec.com>
2022-01-26 12:26:51 +0100

    fix: 🐛 fix styles by prefixing our CSS-In-JS class names

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,20 @@
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'container'
+});
+
 export default () => {
   return <BrowserRouter>
-    <div>
-      <Header />
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </StylesProvider>
   </BrowserRouter>
 }
