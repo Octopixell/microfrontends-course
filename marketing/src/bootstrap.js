70bae2c1e4f5bbc9d7ae85ba178d1cd63dfe84de
Marco Strijker <marco.strijker@ortec.com>
2022-01-27 10:44:20 +0100

    feat: ✨ start using memory router in sub apps and sync history between container and sub apps

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (element) => {
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     element,
   )
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
   const element = document.getElementById('_marketing-dev-root');
   
   if (element) {
-    mount(element);
+    mount(element, { 
+      defaultHistory: createBrowserHistory() 
+    });
   }
 }
 
